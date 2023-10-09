f012030aa4a84f84b105535e9f8f46ee9379a165
Arman Azarnik <armanazarnik@gmail.com>
2023-10-09 14:35:52 +0330

    V 1.0

--- CMD_Fibonacci_Loop.java
+++ CMD_Fibonacci_Loop.java
@@ -10,21 +10,20 @@ import java.util.Scanner;
 public class CMD_Fibonacci_Loop{
 
     /** main class for interacting with the user */
-    public static void main(String args []){
+    public static void main(String[] args){
 
         /** while loop to keep the program running */
         while(true){
             System.out.println("Please enter the number of fibonacci series :");
             Scanner input = new Scanner(System.in);
 
-            /** reading the next line of text and storing it in a string variable*/
+            /** reading the next integer number and storing it in an integer variable*/
             int number_Of_Series = input.nextInt();
-
             System.out.println("Your Fibonacci serie :");
 
-            /** passing the number_Of_Series to Fibonacci_Serie_Generator_Loop method and storing the result in result array of integers */
-            int [] result = Fibonacci_Serie_Generator_Loop(number_Of_Series);
-            int_Array_printer(result);
+            /** passing the number_Of_Series to Fibonacci_Serie_Generator_Loop method and storing the result in result array of long data type */
+            long[] result = Fibonacci_Serie_Generator_Loop(number_Of_Series);
+            long_Array_printer(result);
             System.out.println("**************************************************");
         }
     } 
@@ -32,19 +31,19 @@ public class CMD_Fibonacci_Loop{
     /** 
      * method for generating the Fibonacci series with a for loop.
      * @param number  int number of series
-     * @return Fibonacci_Series    an array of Fibonacci serie integers   
+     * @return Fibonacci_Series    a long data type array of Fibonacci serie integers   
      * <pre>
      *      Fibonacci_Serie_Generator_Loop(0); // Output: []
      *      Fibonacci_Serie_Generator_Loop(6); // Output: [0, 1, 1, 2, 3, 5]
      * </pre>
     */
-    public static int[] Fibonacci_Serie_Generator_Loop(int number){
+    public static long[] Fibonacci_Serie_Generator_Loop(int number){
 
         /** initializing an empty array with lenght of number */
-        int[] Fibonacci_Series = new int[number];
-        int first = 0;
-        int second = 1;
-        int third;
+        long[] Fibonacci_Series = new long[number];
+        long first = 0;
+        long second = 1;
+        long third;
         for(int i=0; i<number; i++){
             Fibonacci_Series[i] = first;
             third = first+second;
@@ -55,17 +54,17 @@ public class CMD_Fibonacci_Loop{
     }
 
     /** 
-     * method for printing each array integer element in a line.
-     * @param array  array of integers
+     * method for printing each array long data type element in a line.
+     * @param array  array of long data type
      * <pre>
-     *      int[] array_1 = [];
-     *      int[] array_2 = [3, 7]
-     *      int_Array_printer(array_1); // Output: 
-     *      int_Array_printer(array_2); // Output: 3
+     *      long[] array_1 = [];
+     *      long[] array_2 = [3, 7]
+     *      long_Array_printer(array_1); // Output: 
+     *      long_Array_printer(array_2); // Output: 3
      *                                             7
      * </pre>
     */
-    public static void int_Array_printer(int[] array){
+    public static void long_Array_printer(long[] array){
         for(int i=0; i<array.length; i++){
             System.out.println(array[i]);
         }
